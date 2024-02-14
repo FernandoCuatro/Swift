6bcdd5d09e0be70c7787d13f4ba0dc718081c45e
FernandoCuatro <fernandocuatrorivera@gmail.com>
2024-02-13 20:40:46 -0600

    Primeras clases para Swift

---
+++ Clases/ProyectoConsola/ProyectoConsola/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProyectoConsola
+//
+//  Created by Fernando Cuatro on 2/8/24.
+//  Copyright © 2024 Fernando Cuatro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
