fcb0b9fdddc41e64e3dc39b82ecc753e60615fc6
FernandoCuatro <fernandocuatrorivera@gmail.com>
2024-03-02 23:07:38 -0600

    SwiftUI Framework

---
+++ Cursos/SwiftUI/SwiftUIFramework/SwiftUIFramework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFramework
+//
+//  Created by Fernando Cuatro on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola mundo")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cursos/SwiftUI/SwiftUIFramework/SwiftUIFramework/SwiftUIFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFrameworkApp.swift
+//  SwiftUIFramework
+//
+//  Created by Fernando Cuatro on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
