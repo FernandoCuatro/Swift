b88054dba494638ecfc21a241ed2b111ad042c21
FernandoCuatro <fernandocuatrorivera@gmail.com>
2024-03-13 22:49:30 -0600

    Actualizar pantallas

--- Cursos/SwiftUI/SwiftUIListas/SwiftUIListas/ListDetailView.swift
+++ Cursos/SwiftUI/SwiftUIListas/SwiftUIListas/ListDetailView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ListDetailView: View {
-    
     // Con esta variable recogemos los datos
     // para mostrarlos en los detalles
     // recibimos un tipo objeto
     var programmer: Programmer
+    
+    // Creamos una varibale para indentificar si el prgroamador
+    // es favorito o no
+    // Se usa el State porque puede mutar
+    // Aqui usamos el Binding porque es un variable que a llegado desde otra clase
+    // a esta y puede ser modificada
+    @Binding var favorite: Bool
     
     var body: some View {
         // Creamos un VStack vertical
@@ -26,7 +32,28 @@ struct ListDetailView: View {
                 .overlay(Circle().stroke(Color.black, lineWidth: 4))
                 .shadow(color: Color.gray, radius: 5)
             
-            Text(programmer.name).font(.largeTitle)
+            // Vamos a agregar la estrellita
+            // Lo metemos en un HStack para que funcione
+            HStack {
+                Text(programmer.name).font(.largeTitle)
+                
+                // El boton lo creamos de esta forma
+                // Para poder aniadierle una accion
+                Button {
+                    // Con esto cambiamos el valor
+                    favorite.toggle()
+                } label: {
+                    // Pintamos la estrella si es favorito o no
+                    if favorite {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                    } else {
+                        Image(systemName: "star")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+            
             Text(programmer.languages).font(.title)
             
             Spacer()
@@ -37,7 +64,8 @@ struct ListDetailView: View {
 struct ListDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ListDetailView(
-            programmer: Programmer(id: 1, name: "Fernando Cuatro", languages: "PHP, Js, C#, Java", avatar: Image(systemName: "person.fill"), favorite: true)
+            programmer: Programmer(id: 1, name: "Fernando Cuatro", languages: "PHP, Js, C#, Java", avatar: Image(systemName: "person.fill"), favorite: true),
+            favorite: .constant(false)
         )
     }
 }
--- Cursos/SwiftUI/SwiftUIListas/SwiftUIListas/ListView.swift
+++ Cursos/SwiftUI/SwiftUIListas/SwiftUIListas/ListView.swift
@@ -7,55 +7,67 @@
 
 import SwiftUI
 
-// Creamos una lista de programadores
-private let programmers = [
-    Programmer(id: 1, name: "Fernando Cuatro", languages: "PHP, Js, C#, Java", avatar: Image(systemName: "person"), favorite: true),
-    Programmer(id: 2, name: "Programador 1", languages: "PHP", avatar: Image(systemName: "person.fill"), favorite: false),
-    Programmer(id: 3, name: "Programador 2", languages: "Js", avatar: Image(systemName: "person.fill"), favorite: false),
-    Programmer(id: 4, name: "Programador 3", languages: "C#", avatar: Image(systemName: "person.fill"), favorite: false),
-    Programmer(id: 5, name: "Programador 4", languages: "Java", avatar: Image(systemName: "person.fill"), favorite: false),
-    Programmer(id: 6, name: "Programador 5", languages: "Imprementador", avatar: Image(systemName: "person.fill"), favorite: false)
-]
+// Tenemos que hacer cambios por los estados que pueden ser variables
+// tenemos que conseguir que el valor favorito de un programador sea mutable
+// para que cuando cambie su valor en una pantalla sea reflejado en la otra
+// Creamos un modelo de dato
+final class ProgrammersMoldelData: ObservableObject {
+    // Con el publicher los publicamos y de let a var para que no sean constantes
+    
+    // Creamos una lista de programadores
+    @Published var programmers = [
+        Programmer(id: 0, name: "Fernando Cuatro", languages: "PHP, Js, C#, Java", avatar: Image(systemName: "person"), favorite: true),
+        Programmer(id: 1, name: "Programador 1", languages: "PHP", avatar: Image(systemName: "person.fill"), favorite: false),
+        Programmer(id: 2, name: "Programador 2", languages: "Js", avatar: Image(systemName: "person.fill"), favorite: false),
+        Programmer(id: 3, name: "Programador 3", languages: "C#", avatar: Image(systemName: "person.fill"), favorite: false),
+        Programmer(id: 4, name: "Programador 4", languages: "Java", avatar: Image(systemName: "person.fill"), favorite: false),
+        Programmer(id: 5, name: "Programador 5", languages: "Imprementador", avatar: Image(systemName: "person.fill"), favorite: false)
+    ]
+}
 
 struct ListView: View {
+    // Para obtener la data
+    @EnvironmentObject var programmersMoldelData: ProgrammersMoldelData
+    
     // Implementamos un filtrado para la lista
     @State private var showFavorites = false
     // Esto sera un listado de programadores mas grande o mas pequenio
     // Si son los vaoritos o todos
     private var filteredProgrammers: [Programmer] {
-        return programmers.filter { programmer in
+        return programmersMoldelData.programmers.filter { programmer in
             return !showFavorites || programmer.favorite
         }
     }
         
     var body: some View {
-        // Agregamos un interruptor
-        VStack {
-            Toggle(isOn: $showFavorites) {
-                Text("Mostrar favoritos")
-            }.padding()
-            
-            // Este componente permite que toda la pantalla
-            // Tenga navegacion
-            NavigationView {
+        // Este componente permite que toda la pantalla
+        // Tenga navegacion
+        NavigationView {
+            // Agregamos un interruptor
+            VStack {
+                Toggle(isOn: $showFavorites) {
+                    Text("Mostrar favoritos")
+                }.padding()
+                
                 // Para pintar una lista
                 List (filteredProgrammers, id: \.id) {
                     // Vamos a usar el Navigation link
                     // Para irnos para otra pantalla
                     Programmer in
-                    NavigationLink ( destination: ListDetailView(programmer: Programmer) ) {
+                    NavigationLink ( destination: ListDetailView(programmer: Programmer,
+                                                                 favorite: $programmersMoldelData.programmers[Programmer.id].favorite ) ) {
                         RowView(programmer: Programmer)
                     }
                 }
-                // Ahora demosle un titulo
-                .navigationTitle("Programmers")
             }
+            // Ahora demosle un titulo
+            .navigationTitle("Programmers")
         }
     }
 }
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        ListView().environmentObject(ProgrammersMoldelData())
     }
 }
